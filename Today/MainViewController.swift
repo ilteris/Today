17b4e696825cbda8a06fa7d30d84b88dc2032b01
ilteris <ilteris@gmail.com>
2015-04-12 19:19:55 -0400

    add the bottom view to the mainviewcontroller and tie the services and update realtime

--- Today/MainViewController.swift
+++ Today/MainViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 import Snap
 
-class MainViewController: UIViewController, UITableViewDataSource {
+
+class MainViewController: UIViewController, UITableViewDataSource, CurrentLocation, ForecastIO {
 
     let kCellId = "blurbCell"
     
+
     let tableView = UITableView(frame: CGRectZero, style: .Plain)
+
+    let bottomView:UIImageView =   {
+        let view = UIImageView(image: UIImage(named: "bottom_bg"))
+        view.backgroundColor = UIColor.clearColor()
+        return view
+    }()
     
+    lazy var weatherView: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "AvenirNext-Regular", size: 14)
+        label.textColor = UIColor.whiteColor()
+        label.textAlignment = .Left
+        return label
+        }()
+    
+    let iconView:UIImageView =   {
+        let view = UIImageView()
+        view.backgroundColor = UIColor.clearColor()
+        return view
+        }()
+    
+    lazy var whatHappenedText: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "AvenirNext-UltraLight", size: 35)
+        label.textColor = UIColor.whiteColor()
+        label.textAlignment = .Center
+        label.text = "WHAT HAPPENED TODAY?"
+        label.numberOfLines = 2
+
+        label.layer.shadowColor = UIColor.blackColor().CGColor
+        label.layer.shadowOffset = CGSizeMake(1, 1)
+        label.layer.shadowOpacity = 0.4;
+        label.layer.shadowRadius = 2;
+   
+        
+        return label
+        }()
+    
+    
+    
+    let locationService = LocationService()
+    let weatherService = WeatherService()
     
     
     convenience init()
@@ -25,6 +68,7 @@ class MainViewController: UIViewController, UITableViewDataSource {
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
     }
     
     required init(coder aDecoder: NSCoder) {
@@ -37,15 +81,62 @@ class MainViewController: UIViewController, UITableViewDataSource {
         view.backgroundColor = UIColor.whiteColor()
         let superview = view
         
+     //   superview.addSubview(UIImageView(image: UIImage(named: "Reference")))
+
+        
+
+        
+        
         superview.addSubview(tableView)
+        superview.addSubview(bottomView)
+        superview.addSubview(weatherView)
+        superview.addSubview(iconView)
+        superview.addSubview(whatHappenedText)
+        
         tableView.dataSource = self
+        
         tableView.rowHeight = 80
         tableView.snp_makeConstraints { make in
             make.width.equalTo(superview.snp_width)
-            make.bottom.equalTo(superview.snp_bottom)
+            make.bottom.equalTo(superview.snp_centerY)
             make.top.equalTo(0)
             make.left.equalTo(0)
         }
+        
+        bottomView.snp_makeConstraints { make in
+            make.width.equalTo(superview.snp_width)
+            make.top.equalTo(self.tableView.snp_bottom).offset(-8)
+            make.left.equalTo(0)
+        }
+        
+        
+        
+        iconView.snp_makeConstraints { make in
+            make.top.equalTo(self.bottomView.snp_top).offset(20)
+            make.left.equalTo(10)
+        }
+
+        
+        
+        
+        weatherView.snp_makeConstraints { make in
+            make.left.equalTo(self.iconView.snp_right).offset(20)
+            make.centerY.equalTo(self.iconView.snp_centerY)
+
+        }
+        
+        whatHappenedText.snp_makeConstraints { make in
+            make.width.equalTo(self.view.snp_width)
+            make.centerY.equalTo(self.bottomView.snp_centerY)
+            make.centerX.equalTo(self.bottomView.snp_centerX)
+            
+        }
+        
+        
+        
+        
+        
+        
     }
     
     
@@ -57,16 +148,65 @@ class MainViewController: UIViewController, UITableViewDataSource {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let blurb = BlurbsManager.sharedInstance.blurbs[indexPath.row]
         let cell = tableView.dequeueReusableCellWithIdentifier("blurbCell") as! BlurbTableCell
+        cell.backgroundColor = UIColor.clearColor()
+
+        cell.setBlurbSummary(blurb)
+
         return cell
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.registerClass(BlurbTableCell.classForCoder(), forCellReuseIdentifier: kCellId)
+        
+        locationService.delegate = self
 
+        
+        tableView.registerClass(BlurbTableCell.classForCoder(), forCellReuseIdentifier: kCellId)
+        tableView.separatorStyle = .None
+        tableView.backgroundColor = UIColor.clearColor()
+        
+
+        //set the delegate here.
+       
+
+        
+        
+        for familyName in UIFont.familyNames() {
+            if let fn = familyName as? String {
+                for font in UIFont.fontNamesForFamilyName(fn) {
+                    println("font: \(font)")
+                }
+            }
+        }
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    /// LocationService delegate method
+    func updatedLocation(lat:String, lon:String) {
+        println("Latitude = \(lon)")
+        println("Longitude = \(lat)")
+   
+        weatherService.delegate = self;
+        weatherService.getCurrentWeatherData(lat, lat: lon)
+        
+    }
+    
+    /// WeatherService delegate methods
+    func currentWeatherData(weather:CurrentWeather) {
+        weatherView.text = String(weather.temperature) + "°" + " " + weather.summary
+        self.iconView.image = weather.icon!
+    }
+    
+    func failedGettingCurrentData() {
+        
+    }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
